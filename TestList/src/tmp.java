78c5adba9a19786aa54ce40e939d17dc73ca33f9
JameyWoo <2622075127wjh@gmail.com>
2019-07-03 16:52:03 +0800

    调整

---
+++ TestList/src/tmp.java
@@ -0,0 +1,6 @@
+/**
+ * @author 姬小野
+ * @data 2019/7/3 上午11:18
+ */
+public class tmp {
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Fuck the World!");
+        test();
+        Login test = new Login();
+        test.getConnection();
+    }
+
+    public static void test() { System.out.println("the hello!");}
+}
